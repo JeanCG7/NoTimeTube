ad0f6a8f4f9a2ddcb3ae70bc0ad509295ba6ffdd
Jean <jean@forlogic.net>
2019-05-02 21:41:08 -0300

    removing conn

--- mongooseConfig.js
+++ mongooseConfig.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://<jeantube>:<tube96210201>@ds149806.mlab.com:49806/notimetube';
+let dev_db_url = 'mongodb://<>:<>@ds149806.mlab.com:49806/notimetube';
 
 let mongoDB = process.env.mongoDB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true});
