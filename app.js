49bdc8006c71c7a53cb6e319d252c7feec202253
Jean <jean@forlogic.net>
2019-05-02 23:26:33 -0300

    user create

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose');
-
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/user.route');
+var userRouter = require('./routes/user.route');
 
-let dev_db_url = 'mongodb://<jeantube>:<tube96210201>@ds149806.mlab.com:49806/notimetube';
-
-let mongoDB = process.env.mongoDB_URI || dev_db_url;
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+let dev_db_url = require('./mongooseConfig').url;
+let mongoDB = dev_db_url;
+mongoose.connect(mongoDB, {useNewUrlParser: true}).catch((reason) => {
+  console.log('Unable to connect to the mongodb instance. Error: ', reason);
+});
 var app = express();
 
 // view engine setup
@@ -20,13 +21,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +40,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log('a');
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- mongooseConfig.js
+++ mongooseConfig.js
@@ -1,8 +1,1 @@
-const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://<>:<>@ds149806.mlab.com:49806/notimetube';
-
-let mongoDB = process.env.mongoDB_URI || dev_db_url;
-mongoose.connect(mongoDB, {useNewUrlParser: true});
-mongoose.Promise = global.Promise;
-
-module.exports = { mongoose };
+module.exports = {  url:'mongodb://user:password@ds149806.mlab.com:49806/notimetube' };
