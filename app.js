0f10a2cb732afd592a80408c7071c5f7ce7dc32d
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-05-06 16:30:35 -0300

    Adicionando estrutura de partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user.route');
@@ -20,6 +21,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(logger('dev'));
 app.use(express.json());
