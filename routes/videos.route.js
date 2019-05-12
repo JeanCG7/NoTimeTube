d3a1c48e0673e496ad13aa9a92939d4ec65e719e
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-05-12 15:09:57 -0300

    Adicionando formulário de upload

--- routes/videos.route.js
+++ routes/videos.route.js
@@ -1,22 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const auth = require('../utils/auth');
+const videoController = require('../controllers/video.controller');
 
 router.get('/', auth.required, function(req, res) {
-    res.render('videos/list-videos');
+  res.render('videos/list-videos');
 });
 
 router.get('/upload', function(req, res) {
-    res.render('videos/upload-videos');
+  res.render('videos/upload-videos');
 });
 
+router.post('/upload', videoController.upload);
+
 router.get('/search', function(req, res) {
-    res.render('videos/search-videos');
+  res.render('videos/search-videos');
 });
 
 router.get('/detail', function(req, res) {
-    res.render('videos/detail-videos');
+  res.render('videos/detail-videos');
 });
 
 module.exports = router;
